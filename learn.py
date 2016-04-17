75b7f0ea4d6c5ce4d40eed7bdb683b8feac3fe2d
Oliver Widder <oliver.widder@gmail.com>
2016-04-17 14:39:26 +0200

    bugfix

--- learn.py
+++
@@ -1,9 +0,0 @@
-def f(a, b):
-    def g(c):
-        print(a*b + c)
-
-    return g
-
-
-f(3,4)(10)
-f(3,4)(20)
--- read.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-fi = open(sys.argv[1], "r")
-
-line = fi.readline()
-
-print(line)
